e83d6e195fa238bca7d7ce316ee6a278f8a84a70
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2025-07-07 16:24:17 +0200

    docs: update project files

---
+++ setting.js
@@ -0,0 +1,5 @@
+const GAME_SETTINGS = {
+  FLIP_DELAY: 1000,           // Задержка закрытия карточек
+  SHUFFLE_CARDS: true,        // Перемешивание при старте
+  DEBUG_MODE: false           // Режим отладки
+};
