37a7154c27542ca3379cc8bbace1ef72e83113cc
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2025-07-31 21:18:47 +0200

    feat: restructure game architecture with unified state and new components

--- js/setting.js
+++ js/setting.js
@@ -1,5 +1,6 @@
 const GAME_SETTINGS = {
   FLIP_DELAY: 800,           // Задержка закрытия карточек
   SHUFFLE_CARDS: true,        // Перемешивание при старте
-  DEBUG_MODE: false           // Режим отладки
+  DEBUG_MODE: false,          // Режим отладки
+  LIVES_COUNT: 3,             // Количество жизней
 };
