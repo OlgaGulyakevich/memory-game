70bcab6dd79555de1c476660f8a53582cc9d6b04
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2025-09-02 17:12:37 +0200

    change code theme

--- js/setting.js
+++ js/setting.js
@@ -2,7 +2,8 @@ const GAME_SETTINGS = {
   FLIP_DELAY: 900,           // Задержка закрытия карточек
   SHUFFLE_CARDS: true,        // Перемешивание при старте
   DEBUG_MODE: false,          // Режим отладки
-  LIVES_COUNT: 3,             // Количество жизней
+  LIVES_COUNT: 300,          // Количество жизней (большое значение для соответствия ТЗ)
+  THEMES: ['flowers', 'cats', 'cars']  // Доступные темы
 };
 
 // Экраны приложения
