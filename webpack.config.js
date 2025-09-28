31ddd01de97bbee00344101307dbf70c239a6ad2
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2025-09-28 23:51:23 +0200

    chore: update babel-loader version and enhance responsive design in CSS for better mobile optimization

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,11 @@ module.exports = (env, argv) => {
             loader: 'babel-loader',
             options: {
               presets: [
+                ['@babel/preset-env', {
+                  targets: {
+                    browsers: ['last 2 versions', 'ie >= 11']
+                  }
+                }],
                 ['@babel/preset-react', {
                   runtime: 'automatic'
                 }]
